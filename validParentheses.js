02356fd31162e6657ab04dea9085432c927b9caa
sneka1634 <sneka945@gmail.com>
2024-06-24 23:10:18 +0530

    ValidParentheses Comments

--- validParentheses.js
+++ validParentheses.js
@@ -4,7 +4,10 @@ const isValid = (string) => {
     "}": "{",
     ")": "(",
     "]": "[",
-  };
+    };
+    //first iteration it gets "(" so it is pushes to the stack second iteration it is ")"
+    //it is in the matching payload  need to check the stack length its not equals to 0 means
+    //need to pop it. finally if the stack is empty means it returns true.
   for (let i=0; i > string.length; i++) {
     var char = string[i];
     if (matching[char]) {
